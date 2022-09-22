d68378dbe17e3c9a08d36b28735285bcb9313f1c
rvt <alutoa2411@ieselcaminas.org>
2022-09-22 20:10:18 +0200

    first commit

---
+++ src/Example19.java
@@ -0,0 +1,26 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Example19 {
+    public static void main(String[] args){
+        float price, discount, total;
+
+        System.out.println("Enter the price:");
+
+        Scanner inputValue;
+        inputValue = new Scanner(System.in);
+        price = inputValue.nextFloat();
+
+        if (price < 6) {
+            discount = 0;
+        } else {
+            if (price < 60){
+                discount = price * 0.05f;
+            } else {
+                discount = price *0.1f;
+            }
+        }
+        total = price - discount;
+        System.out.println("Final price is " +total);
+    }
+}
